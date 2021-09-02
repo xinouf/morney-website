f7081e5ce0167718e507c58cfde18bcee6e1fcf1
xinouf <852618183@qq.com>
2021-09-02 14:17:27 +0800

    deploy

---
+++ precache-manifest.f9d912573cc6a5c4f61bc11a59ff6274.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6455ddc44c71d8f45c",
+    "url": "/morney-website/css/app.8b611e98.css"
+  },
+  {
+    "revision": "bbc53c0c940044c15b8969622c780c54",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "bf6455ddc44c71d8f45c",
+    "url": "/morney-website/js/app.d4e05f15.js"
+  },
+  {
+    "revision": "3da9d4c0600775b661b2",
+    "url": "/morney-website/js/chunk-vendors.ace1c3ae.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "d00bf803d9f129d2056bf6210463487d",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
